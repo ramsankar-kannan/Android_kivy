728440f9011727653acd604752fcf09c716f393a
ramsankar <ramsankar.k@ionixxtech.com>
2020-02-06 16:09:28 +0530

    Signup screen design

--- core.py
+++ core.py
@@ -1,16 +1,11 @@
-import kivy
-
 from kivy.app import App
 from kivy.lang.builder import Builder
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.textinput import TextInput
-from kivy.uix.button import Button
-from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
 
 
-class SignupScreen(GridLayout):
+class SignupScreen(BoxLayout):
     user_id = ObjectProperty(None)
     user_password = ObjectProperty(None)
 
